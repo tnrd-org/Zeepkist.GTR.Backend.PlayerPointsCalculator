f8d23a74527caba19ccb45f0ac7477c3ff18d371
Christiaan Bloemendaal <github@tnrd.net>
2023-10-22 14:59:35 +0200

    fix: naming for logging

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ internal class Program
             {
                 configuration
                     .Enrich.FromLogContext()
-                    .Enrich.WithProperty("Source", "LevelScoreCalculator")
+                    .Enrich.WithProperty("Source", "PlayerPointsCalculator")
                     .MinimumLevel.Debug()
                     .WriteTo.Seq(context.Configuration["Seq:Url"], apiKey: context.Configuration["Seq:Key"])
                     .WriteTo.Console();
